813729a39279ff505e076e165627462fa232eab5
Pha <110311665+nguyentpha1999@users.noreply.github.com>
2022-09-14 23:45:35 -0700

    website

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+var swiper = new Swiper(".anime-slider", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
